3572a8f1b4d216c733392f9e2654bcf8220eecba
Akın Burak Yazlık <36696084+Manajuri@users.noreply.github.com>
2020-08-14 14:06:09 +0300

    new 14.08.2020 14.06

--- template-master/server/app.js
+++ template-master/server/app.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get("/todos",(req,res)=>{
+  //grt todoos from db
+  let todoos = [{text:"çalış",complete:false},{text:"yat",complete:true}]
+  res.json(todoos)
+})
+app.post("/account/login",(req,res)=>{
+  console.log(req.body)
+  res.json({user:"bahadır"})
 })
 
 app.listen(port, () => {
